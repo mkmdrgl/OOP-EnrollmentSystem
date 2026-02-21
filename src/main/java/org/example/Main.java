82f227ef917717d338ee62519fc1e792249e48ee
Mika Madrigal <mikamadrigal10@gmail.com>
2026-02-21 11:51:18 +0800

    Created model and service packages, added Array List and CRUD in StudentEnrollment, imported model and service packages in Main, commented setters in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,29 @@
 package org.example;
+import org.example.model.Student;
+import org.example.model.Course;
+import org.example.service.StudentEnrollment;
 
 public class Main {
     public static void main(String[] args) {
 
-        Student student = new Student();
-        student.setstudentID("000123");
-        student.setstudentName("John Doe");
-        student.setprogram("BS Information Technology");
-        student.display();
-        System.out.print("\n");
-
-        Student student2 = new Student();
-        student2.setstudentID("000124");
-        student2.setstudentName("Jane Doe");
-        student2.setprogram("BS Information Technology");
-        student2.display();
-        System.out.print("\n");
-
-        Course course = new Course();
-        course.setcourseID("000001");
-        course.setcourseName("Integrative Programming");
-        course.setprogram("BS Information Technology");
-        course.display();
-
+//        Student student = new Student();
+//        student.setstudentID("000123");
+//        student.setstudentName("John Doe");
+//        student.setprogram("BS Information Technology");
+//        student.display();
+//        System.out.print("\n");
+//
+//        Student student2 = new Student();
+//        student2.setstudentID("000124");
+//        student2.setstudentName("Jane Doe");
+//        student2.setprogram("BS Information Technology");
+//        student2.display();
+//        System.out.print("\n");
+//
+//        Course course = new Course();
+//        course.setcourseID("000001");
+//        course.setcourseName("Integrative Programming");
+//        course.setprogram("BS Information Technology");
+//        course.display();
     }
 }
---
+++ src/main/java/org/example/service/StudentEnrollment.java
@@ -0,0 +1,36 @@
+package org.example.service;
+import org.example.model.Student;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class StudentEnrollment {
+    private Scanner input = new Scanner(System.in);
+    private ArrayList<Student> studentsList = new ArrayList<>();
+
+    // Create
+    public void addStudent(Student student) {
+        studentsList.add(student);
+    }
+
+    // Read
+    public void displayAll(Student student) {
+        System.out.print(studentsList);
+    }
+
+    // Update
+    public void updateStudent(Student student) {
+        for (int i = 0; i < studentsList.size(); i++) {
+            System.out.print("Enter new name: ");
+            String name = input.nextLine();
+
+            System.out.println("Enter new program: ");
+            String program = input.nextLine();
+
+            studentsList.set(i, new Student(student.getstudentID(), name, program));
+            break;
+        }
+    }
+
+    // Remove
+
+}
