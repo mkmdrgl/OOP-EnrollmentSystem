8f17b84ed52d907d2209e7980135b935ad02e99a
Mika Madrigal <mikamadrigal10@gmail.com>
2026-03-07 11:58:13 +0800

    Created markdown file for Inheritance

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# OOP Enrollment System
+
+---
+
+**Author**: Mika Madrigal
+
+**1. Description**: Inheritance
+
+![scan.pn](src/main/resources/Images/Screenshot 2026-03-07 114928.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,17 @@ public class Main {
         Student student1 = new Student(2024390901, "Mikaela Alexa A. Madrigal");
         Instructor instructor1 = new Instructor (2024109093, "Erika Trixie P. Dirilo");
 
-        System.out.println(student1.getID());
-        System.out.println(student1.getName());
+        System.out.println("Student ID: " + student1.getID());
+        System.out.println("Student Name: " + student1.getName());
         student1.setProgram("BS Information Technology");
-        System.out.println(student1.getProgram());
+        System.out.println("Student Program: " + student1.getProgram());
 
-        System.out.println(instructor1.getID());
-        System.out.println(instructor1.getName());
+        System.out.print("\n");
+
+        System.out.println("Instructor ID: " + instructor1.getID());
+        System.out.println("Instructor Name: " + instructor1.getName());
         instructor1.setCourses("Integrative Programming and Technologies");
-        System.out.println(instructor1.getCourses());
+        System.out.println("Instructor Course: " + instructor1.getCourses());
 
     }
 }
