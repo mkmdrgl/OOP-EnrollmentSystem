731f63b6c4f51b131aefb4fc7559ee850ddcb96f
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 21:26:53 +0800

    Added EnrollmentServiceImpl to implement IEnrollmentService. Add view department hierarchy to EnrollmentServiceImpl later

---
+++ src/main/java/org/example/Implementations/EnrollmentServiceImpl.java
@@ -0,0 +1,20 @@
+package org.example.Implementations;
+
+import org.example.Entities.Student;
+import org.example.Entities.Course;
+import org.example.Entities.Instructor;
+import org.example.Interfaces.IEnrollmentService;
+
+import java.util.ArrayList;
+
+public class EnrollmentServiceImpl implements IEnrollmentService {
+    private ArrayList<Student> studentList = new ArrayList<>();
+    private ArrayList<Course> courseList = new ArrayList<>();
+    private ArrayList<Instructor> instructorList = new ArrayList<>();
+
+    public void enrollStudentInSection(Student student, Course course) {
+        student.getEnrolledCourses().add(course);
+        System.out.println("Student " + student.getName() + " enrolled in " + course.getcourseName());
+    }
+
+}
--- src/main/java/org/example/Interfaces/IEnrollmentService.java
+++ src/main/java/org/example/Interfaces/IEnrollmentService.java
@@ -2,17 +2,7 @@ package org.example.Interfaces;
 
 import org.example.Entities.Student;
 import org.example.Entities.Course;
-import org.example.Entities.Instructor;
-import java.util.ArrayList;
 
-public class IEnrollmentService {
-    private ArrayList<Student> studentList = new ArrayList<>();
-    private ArrayList<Course> courseList = new ArrayList<>();
-    private ArrayList<Instructor> instructorList = new ArrayList<>();
-
-    public void enrollStudentInSection(Student student, Course course) {
-        student.getEnrolledCourses().add(course);
-        System.out.println("Student " + student.getName() + " enrolled in " + course.getcourseName());
-    }
-
+public interface IEnrollmentService {
+    void enrollStudentInSection(Student student, Course course);
 }
