b7721ed5b9bf4b6d980656030d9c5c4b94354f97
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 22:21:12 +0800

    Added BeforeEach and Test for making payment, if tuition fee is fully paid or not, and calculate remaining balance

--- src/test/java/org/example/Implementations/TuitionServiceImplTest.java
+++ src/test/java/org/example/Implementations/TuitionServiceImplTest.java
@@ -12,4 +12,33 @@ class TuitionFeePaymentTest {
     void setup() {
         tuitionService = new TuitionServiceImpl();
     }
+
+    @Test
+    void shouldMakeAPayment() {
+        double totalTuition = tuitionService.calculateFee(3, 0.10);
+        double remainingBalance = tuitionService.makePayment(totalTuition, 1000.0);
+        assertEquals(1700.0, remainingBalance, "The remaining balance should be 1700 after paying 1000");
+    }
+
+    @Test
+    void shouldCheckIfTheTuitionFeeIsNotFullyPaid() {
+        double totalTuition = tuitionService.calculateFee(3, 0.10);
+        double remainingBalance = tuitionService.makePayment(totalTuition, 1000.0);
+        assertTrue(remainingBalance > 0, "Fee should not be fully paid when balance is greater than 0");
+    }
+
+    @Test
+    void shouldCheckIfTheTuitionFeeIsFullyPaid() {
+        double totalTuition = tuitionService.calculateFee(3, 0.10);
+        double remainingBalance = tuitionService.makePayment(totalTuition, 2700.0);
+        assertEquals(0.0, remainingBalance, "Fee should be fully paid (0.0 balance)");
+    }
+
+    @Test
+    void shouldCalculateRemainingBalanceCorrectly() {
+        double totalTuition = 5000.0;
+        double amountPaidSoFar = 2000.0;
+        double result = tuitionService.getRemainingBalance(totalTuition, amountPaidSoFar);
+        assertEquals(3000.0, result);
+    }
 }
