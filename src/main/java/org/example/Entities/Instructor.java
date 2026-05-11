158a8651ae0f3d50c31ff105f0363c3dceb76bed
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 18:10:07 +0800

    Added constructors, and getters and setters in Instructor

--- src/main/java/org/example/Entities/Instructor.java
+++ src/main/java/org/example/Entities/Instructor.java
@@ -1,4 +1,30 @@
 package org.example.Entities;
 
+import java.util.ArrayList;
+
 public class Instructor {
+    private String instructorID;
+    private String instructorName;
+
+    public Instructor(String instructorID, String instructorName) {
+        this.instructorID = instructorID;
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorID() {
+        return instructorID;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorID(String instructorID) {
+        this.instructorID = instructorID;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
 }
