0d9f9023e15314fc784c38dba3b4af783204fef0
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 14:31:56 +0800

    Renamed 'model' folder to 'Entities' and added aditional classes: Instructor, Section, and Tuition Fee Payment

---
+++ src/main/java/org/example/Entities/Department.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+public class Department {
+    private String id;
+    private String department;
+
+    public Department(String id, String department) {
+        this.id = id;
+        this.department = department;
+    }
+}
