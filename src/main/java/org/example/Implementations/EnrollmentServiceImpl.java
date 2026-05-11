8fa6037b155176cda4a208c3ce599108f2c3f299
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-12 02:16:32 +0800

    Recovered department hierarchy in Enrollment Service

--- src/main/java/org/example/Implementations/EnrollmentServiceImpl.java
+++ src/main/java/org/example/Implementations/EnrollmentServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.Implementations;
 import org.example.Entities.Student;
 import org.example.Entities.Section;
 import org.example.Entities.Course;
+import org.example.Entities.Department;
 import org.example.Entities.Instructor;
 import org.example.Exceptions.SectionFullExcep;
 import org.example.Interfaces.IEnrollmentService;
@@ -41,4 +42,37 @@ public class EnrollmentServiceImpl implements IEnrollmentService {
             System.out.println();
         }
     }
+
+    public void displayDepartmentHierarchy(Department department) {
+        System.out.println("\n==========================================");
+        System.out.println("DEPARTMENT: " + department.getDepartment());
+        System.out.println("==========================================");
+
+        if (department.getSections().isEmpty()) {
+            System.out.println("   No sections registered in this department.");
+        } else {
+            for (Section section : department.getSections()) {
+                System.out.println("\nSECTION: " + section.getSectionName());
+                System.out.println("------------------------------------------");
+
+                Instructor inst = section.getAssignedInstructor();
+                if (inst != null) {
+                    System.out.println("  ASSIGNED INSTRUCTOR: " + inst.getName());
+                } else {
+                    System.out.println("  ASSIGNED INSTRUCTOR: [No Instructor Assigned]");
+                }
+
+                System.out.println("  ENROLLED STUDENTS (" + section.getEnrolledStudents().size() + "/" + section.getMaxCapacity() + "):");
+                if (section.getEnrolledStudents().isEmpty()) {
+                    System.out.println("    - No students enrolled.");
+                } else {
+                    for (Student student : section.getEnrolledStudents()) {
+                        System.out.println("    - [" + student.getID() + "] " + student.getName() + " (" + student.getProgram() + ")");
+                    }
+                }
+            }
+        }
+        System.out.println("==========================================\n");
+    }
+
 }
