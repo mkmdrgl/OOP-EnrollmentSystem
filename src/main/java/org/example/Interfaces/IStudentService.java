37ea8778989f057024dab34944543deb36bcae29
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 19:30:59 +0800

    Added Interfaces package and IStudentService with Scanner and Array List

---
+++ src/main/java/org/example/Interfaces/IStudentService.java
@@ -0,0 +1,12 @@
+package org.example.Interfaces;
+
+import org.example.Entities.Student;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IStudentService {
+    Scanner input = new Scanner(System.in);
+    private ArrayList<Student> studentList = new ArrayList<>();
+
+}
