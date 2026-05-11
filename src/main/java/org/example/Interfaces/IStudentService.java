40b9aebe9b3301af2de0349c44226aa1fba0b148
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 21:05:00 +0800

    Removed unusued imported packages in IStudentService

--- src/main/java/org/example/Interfaces/IStudentService.java
+++ src/main/java/org/example/Interfaces/IStudentService.java
@@ -1,10 +1,6 @@
 package org.example.Interfaces;
 
 import org.example.Entities.Student;
-import org.example.Entities.Course;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.ArrayList;
 
 public interface IStudentService {
     void addStudent(Student student);
