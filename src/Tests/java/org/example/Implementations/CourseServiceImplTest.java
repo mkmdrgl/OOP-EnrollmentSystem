4a2db0876991f9825375b868f07ee71746c5b15c
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-12 00:42:01 +0800

    Updated test for Course Service

---
+++ src/Tests/java/org/example/Implementations/CourseServiceImplTest.java
@@ -0,0 +1,38 @@
+package org.example.Implementations;
+
+import org.example.Entities.Course;
+import org.example.Exceptions.InvalidInputExcep;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceImplTest {
+    private CourseServiceImpl courseService;
+
+    @BeforeEach
+    void setUp() {
+        courseService = new CourseServiceImpl();
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAddingEmptyID() {
+        Course badCourse = new Course("", "Java", "CS");
+        assertThrows(InvalidInputExcep.class, () -> {
+            courseService.addCourse(badCourse);
+        });
+    }
+
+    @Test
+    void shouldAddCourseSuccessfully() throws InvalidInputExcep {
+        Course validCourse = new Course("CS101", "Java", "CS");
+        courseService.addCourse(validCourse);
+
+        assertEquals(1, courseService.getAllCourses().size());
+    }
+
+    @Test
+    void shouldReturnErrorOnRemovingNonExistentCourse() {
+        String result = courseService.removeCourse("999");
+        assertEquals("Error: Course not found", result);
+    }
+}
