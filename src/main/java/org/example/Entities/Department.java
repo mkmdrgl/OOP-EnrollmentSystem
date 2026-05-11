9e47d41fff31f933c6fb419b251901824fa384e6
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 18:51:34 +0800

    Added constructors, getters and setters, and Array List in Department

--- src/main/java/org/example/Entities/Department.java
+++ src/main/java/org/example/Entities/Department.java
@@ -1,11 +1,34 @@
 package org.example.Entities;
 
-public class Department {
-    private String id;
-    private String department;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Department(String id, String department) {
-        this.id = id;
-        this.department = department;
+public class Department {
+    private String departmentName;
+    private List<Section> sections;
+
+    public Department() {
+        this("Unknown");
+    }
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+        this.sections = new ArrayList<>();
+    }
+
+    public String getDepartment() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
     }
 }
