f5b1b2e987b1bc4b61262f73c579a4b066e00c09
Mika Madrigal <mikamadrigal10@gmail.com>
2026-05-11 18:54:42 +0800

    Added computation for Tuition Fee Payment and getter for Remaining Balance

--- src/main/java/org/example/Entities/TuitionFeePayment.java
+++ src/main/java/org/example/Entities/TuitionFeePayment.java
@@ -1,4 +1,28 @@
 package org.example.Entities;
 
 public class TuitionFeePayment {
+    private final double PRICE_PER_UNIT = 1000.00;
+    private double balance;
+    private double totalTuition;
+
+    public double calculateTuitionFee(int units, double discountRate) {
+        totalTuition = units * PRICE_PER_UNIT;
+
+        if (discountRate != 0) {
+            totalTuition = totalTuition - (totalTuition * discountRate);
+        }
+        return totalTuition;
+    }
+
+    public void makePayment(double amount) {
+        balance = totalTuition - amount;
+    }
+
+    public double getRemainingBalance() {
+        return balance;
+    }
+
+    public boolean isFullyPaid() {
+        return balance == 0 ? true : false;
+    }
 }
